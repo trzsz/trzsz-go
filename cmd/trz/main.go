ddf9b4b054e5af1ae4c7b08c00dcef6d28862d62
Lonny Wong <lonnywong@qq.com>
2024-01-20 20:38:02 +0800

    update copyright

--- cmd/trz/main.go
+++ cmd/trz/main.go
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2023 Lonny Wong <lonnywong@qq.com>
+Copyright (c) 2022-2024 The Trzsz Authors.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -26,6 +26,7 @@ package main
 
 import (
 	"os"
+
 	"github.com/trzsz/trzsz-go/trzsz"
 )
 
