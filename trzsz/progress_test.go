82ad252a93605d3f95548b09ae1d00de1e41bb0e
Lonny Wong <lonnywong@qq.com>
2025-11-01 00:06:20 +0800

    update copyright

--- trzsz/progress_test.go
+++ trzsz/progress_test.go
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2022-2024 The Trzsz Authors.
+Copyright (c) 2022-2025 The Trzsz Authors.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
